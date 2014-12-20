cd0570c60fd4ad4bb558f8b4521659d9aee6854b
John Tantalo <john.tantalo@gmail.com>
2014-12-20 12:33:14 -0800

    format whitespace

--- auth_google.py
+++ auth_google.py
@@ -5,13 +5,13 @@ from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.tools import run_flow, argparser
 
 def main():
-	client_id = sys.argv[1]
-	client_secret = sys.argv[2]
-	scope = sys.argv[3]
-	flow = OAuth2WebServerFlow(client_id, client_secret, scope)
-	storage = Storage('google.json')
-	flags = argparser.parse_args([])
-	run_flow(flow, storage, flags)
+  client_id = sys.argv[1]
+  client_secret = sys.argv[2]
+  scope = sys.argv[3]
+  flow = OAuth2WebServerFlow(client_id, client_secret, scope)
+  storage = Storage('google.json')
+  flags = argparser.parse_args([])
+  run_flow(flow, storage, flags)
 
 if __name__ == '__main__':
-	main()
+  main()
